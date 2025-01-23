658d4e4a2362807f51fee08165600721a89e0c02
sfluegel <simon.fluegel@uni-osnabrueck.de>
2025-01-23 14:11:43 +0100

    improve proteinogenics: glutamine/asparagine priorities, histidine variants, cip code for cysteine

--- chemlog2/classification/proteinogenics_classifier.py
+++ chemlog2/classification/proteinogenics_classifier.py
@@ -26,34 +26,36 @@ def get_proteinogenic_amino_acids(mol: Chem.Mol, amino_ns, carboxy_cs):
                         if len(side_chain_start) == 0:
                             results.append("G")
                             results_atoms.append(aa)
-                        # distinguish R- and L-amino acids, only one side chain per amino acid is allowed
-                        if len(side_chain_start) == 1 and alpha_c.HasProp("_CIPCode") and alpha_c.GetProp("_CIPCode") == "S":
+                        if len(side_chain_start) == 1:
                             start_at = side_chain_start[0].GetIdx()
                             amino_acid = match_side_chain_to_backbone(mol,
                                 side_chains, [n, alpha_c.GetIdx(), carboxy_c.GetIdx(), start_at])
                             if amino_acid is not None:
-                                results.append(amino_acid[0])
-                                results_atoms.append(amino_acid[1])
+                                # distinguish R- and L-amino acids, only one side chain per amino acid is allowed
+                                # L-cysteine has a different CIP code
+                                expected_cip_code = "R" if amino_acid[0] == "C" else "S"
+                                if alpha_c.HasProp("_CIPCode") and alpha_c.GetProp("_CIPCode") == expected_cip_code:
+                                    results.append(amino_acid[0])
+                                    results_atoms.append(aa + amino_acid[1])
     return results, results_atoms
 
 def match_side_chain_to_backbone(mol, side_chains, backbone):
     # assume that backbone has 4 atoms, NC(C)R where the second C belongs to a carboxylic acid derivative
     for side_chain, matches in side_chains.items():
-        if side_chain == "R_iso":
-            side_chain = "R"
-        matches_starting_at = [match for match in matches if backbone[-1] == match[0]]
+        side_chain = side_chain.split("_")[0] # _iso are isomers of the same amino acid
+        matches_starting_at = [match for match in matches if backbone[-1] == match[0] and backbone[1] not in match]
         if len(matches_starting_at) == 1:
             # proline: close ring to amino group
             if side_chain == "P" and matches_starting_at[0][-1] != backbone[0]:
                 continue
             # methionine: be aware of N-formyl-methionine
-            if side_chain == "M":
+            elif side_chain == "M":
                 formyl = mol.GetSubstructMatches(Chem.MolFromSmarts("N[CH1X3]=[OX1]"))
                 formyl = [f for f in formyl if f[0] == backbone[0]]
                 if len(formyl) == 1:
-                    return "fMet", backbone[:2] + list(matches_starting_at[0]) + list(formyl[0])[1:]
+                    return "fMet", list(matches_starting_at[0]) + list(formyl[0])[1:]
             logging.debug(f"{backbone} matches side chain {side_chain} with {matches_starting_at}")
-            return side_chain, backbone[:2] + list(matches_starting_at[0])
+            return side_chain, list(matches_starting_at[0])
 
         elif len(matches_starting_at) > 1:
             logging.warning(f"Found {len(matches_starting_at)} matches (expected max. 1) for "
@@ -65,19 +67,22 @@ side_chains = {
     # -> first, try valine SMARTS and don't get to alanine
     # amino and carboxylic acid groups are underspecified -> we already have those, including their derivatives
     # in peptides -> the lists include the indices in the matches where amino / carboxylic acid residues are expected
-    "W": "[CH2X4][cX3]1[cX3H][nX3][cX3]2[cX3H][cX3H][cX3H][cX3H][cX3]12",  # tryptophan
-    "Y": "[CH2X4][cX3]1[cX3H][cX3H][cX3]([OX2,OX1-])[cX3H][cX3H]1",  # tyrosine
-    "F": "[CH2X4][cX3]1[cX3H][cX3H][cX3H][cX3H][cX3H]1",  # phenylalanine
-    "H": "[CH2X4][cX3]1[cX3H][nX3][cX3H][nX2]1",  # histidine
+    # assume that input is kekulized, no aromaticity
+    "W": "[CH2X4][CX3]1=[CX3H][NX3][CX3]2=[CX3]1[CX3]=[CX3H][CX3H]=[CX3]2",  # tryptophan
+    "Y": "[CH2X4][CX3]1=[CX3H][CX3H]=[CX3]([OX2H])[CX3H]=[CX3H]1",  # tyrosine
+    "F": "[CH2X4][CX3]1=[CX3H][CX3H]=[CX3H][CX3H]=[CX3H]1",  # phenylalanine
+    "H": "[CH2X4][CX3]1=[CX3H][NX3][CX3H][NX2]1",  # histidine
+    "H_iso": "[CH2X4][CX3]1=[CX3H][NX2][CX3H][NX3]1",  # histidine isomer
     "R": ("[CH2X4][CH2X4][CH2X4][NHX3][CH0X3](=[NX3+,NX2])N", [3, 6], []),  # arginine
     "R_iso": ("[CH2X4][CH2X4][CH2X4][NX2]=[CH0X3](N)N", [5, 6], []),  # arginine isomer
     "O": ("[CH2X4][CH2X4][CH2X4][CH2X4]N[CX3][CH1X4]1[NX2]=[CH1X3][CH2X4][CH1X4]([CH3X4])1", [4], [5]), # pyrrolysine
     "K": ("[CH2X4][CH2X4][CH2X4][CH2X4]N", [4], []),  # lysine
     "M": "[CH2X4][CH2X4][SX2][CH3X4]",  # methionine
+    "Q": ("[CH2X4][CH2X4][CX3](=[OX1])[NH2X3]", [4], []),  # glutamine ! last n has to have two H bonds
+    # (otherwise, it would be glutamic acid with an amide bond)
     "E": ("[CH2X4][CH2X4][CX3]", [], [2]),  # glutamic acid
-    "Q": ("[CH2X4][CH2X4][CX3](=[OX1])N", [4], []),  # glutamine
     "P": ("[CH2X4][CH2X4][CH2X4]N", [3], []),  # proline (without ring closed)
-    "N": ("[CH2X4][CX3](=[OX1])N", [3], []),  # asparagine
+    "N": ("[CH2X4][CX3](=[OX1])[NH2X3]", [3], []),  # asparagine ! last n has to have two H bonds
     "D": ("[CH2X4][CX3]", [], [1]),  # aspartic acid
     "I": "[CHX4]([CH3X4])[CH2X4][CH3X4]", # isoleucine
     "L": "[CH2X4][CHX4]([CH3X4])[CH3X4]", # leucine
@@ -106,55 +111,7 @@ def identify_side_chains_smarts(mol, amino_ns, carboxy_cs):
     return side_chain_matches
 
 
-def identify_side_chain(mol, aa):
-    # outdated, incomplete
-    side_chain = [neighbor for neighbor in mol.GetAtomWithIdx(aa[1]).GetNeighbors() if neighbor.GetIdx() not in aa]
-    if len(side_chain) == 0:
-        return "G", aa
-    elif len(side_chain) == 1 and side_chain[0].GetAtomicNum() == 6:
-        aa.append(side_chain[0].GetIdx())
-        print(mol.GetBondBetweenAtoms(aa[0], aa[1]).GetStereo())
-        next_atoms = [neighbor for neighbor in mol.GetAtomWithIdx(aa[-1]).GetNeighbors() if neighbor.GetIdx() not in aa]
-        if len(next_atoms) == 0:
-            return "A", aa
-        elif len(next_atoms) == 1:
-            aa.append(next_atoms[0].GetIdx())
-            next_atoms = [neighbor for neighbor in mol.GetAtomWithIdx(aa[-1]).GetNeighbors() if
-                          neighbor.GetIdx() not in aa]
-            if len(next_atoms) == 0:
-                # serine, cysteine, selenocysteine
-                if mol.GetAtomWithIdx(aa[-1]).GetAtomicNum() == 8:
-                    return "S", aa
-                elif mol.GetAtomWithIdx(aa[-1]).GetAtomicNum() == 16:
-                    return "C", aa
-                elif mol.GetAtomWithIdx(aa[-1]).GetAtomicNum() == 34:
-                    return "U", aa
-            elif mol.GetAtomWithIdx(aa[-1]).GetAtomicNum() == 6:
-                # order is important: e.g. Phenylalanine would also match tyrosine
-                for pattern, aa_code in [("c1cnc2ccccc21", "W"), ("c1ccc([#8H]")]:
-                    for match in mol.GetSubstructMatches(Chem.MolFromSmarts(pattern)):
-                        if aa[-1] in match:
-                            return aa_code, list(set.union(set(aa), set(match)))
-        elif len(next_atoms) == 2:
-            # valine, isoleucine, threonine
-            aa.append(next_atoms[0].GetIdx())
-            aa.append(next_atoms[1].GetIdx())
-            if all(neighbor.GetIdx() in aa for a in next_atoms for neighbor in a.GetNeighbors()):
-                if (sum(a.GetAtomicNum() == 8 for a in next_atoms) == 1
-                        and sum(a.GetAtomicNum() == 6 for a in next_atoms) == 1):
-                    return "T", aa
-                if sum(a.GetAtomicNum() == 6 for a in next_atoms) == 2:
-                    return "V", aa
-            else:
-                next_next_atoms = [neighbor for a in next_atoms for neighbor in a.GetNeighbors()
-                                   if neighbor.GetIdx() not in aa]
-                if (len(next_next_atoms) == 1 and next_next_atoms[0].GetAtomicNum() == 6
-                        and all(neighbor.GetIdx() in aa for neighbor in next_next_atoms[0].GetNeighbors())):
-                    return "I", aa
-        return None
-
-
 if __name__ == "__main__":
-    mol = Chem.MolFromSmiles("C(=O)([C@@H](N)CC1=CNC2=C1C=CC=C2)N[C@H](C(=O)N[C@H](C(=O)O)C(C)C)CC3=CNC4=C3C=CC=C4")
+    mol = Chem.MolFromSmiles("NCC(=O)N[C@@H](Cc1ccccc1)C(O)=O")
     print(get_proteinogenic_amino_acids(mol, get_amino_groups(mol, []), get_carboxy_derivatives(mol)))
     plot_mol(mol)
