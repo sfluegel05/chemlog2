1cc243805f8c327a61cf91e0b16242fdb3df0cce
sfluegel <simon.fluegel@uni-osnabrueck.de>
2025-02-06 09:52:04 +0100

    fix diketopiperazine SMARTS

--- chemlog2/classification/substructure_classifier.py
+++ chemlog2/classification/substructure_classifier.py
@@ -4,7 +4,7 @@ EMERICELLAMIDE_SMARTS = ("[C@H,CH2]1[OX2H0][CX3H0](=[OX1H0])[C@HX4]([CX4H3])[NX3
                          "[CX3H0](=[OX1H0])[C@HX4]([CX4H2][CX4H1]([CX4H3])[CX4H3])[NX3H][CX3H0](=[OX1H0])[C@@HX4]"
                          "([NX3H][CX3H0](=[OX1H0])[CX4H2][NX3H][CX3H0](=[OX1H0])[C@@H,CH2]1)[CX4H]([CX4H3])[CX4H3]")
 
-DIKETOPIPERAZINE_SMARTS = "[CX4H]1[NX3H][CX3H0](=[OX1H0])[CX4H][NX3H][CX3H0]1=[OX1H0]"
+DIKETOPIPERAZINE_SMARTS = "C1[NX3H][CX3H0](=[OX1H0])C[NX3H][CX3H0]1=[OX1H0]"
 
 def apply_smarts(mol, smarts):
     has_match = mol.HasSubstructMatch(Chem.MolFromSmarts(smarts))
@@ -20,6 +20,6 @@ def is_diketopiperazine(mol):
     return apply_smarts(mol, DIKETOPIPERAZINE_SMARTS)
 
 if __name__ == "__main__":
-    mol = Chem.MolFromSmiles("CC(C)C[C@@H]1NC(=O)[C@H](Cc2ccccc2)NC1=O")
+    mol = Chem.MolFromSmiles("O=C1NCC(=O)NC1C")
     print(is_emericellamide(mol))
     print(is_diketopiperazine(mol))
