546fbfcd4039a33b60576b273564f76e71a8ad1e
sfluegel <simon.fluegel@uni-osnabrueck.de>
2025-03-26 18:23:59 +0100

    add README to setup, update license

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='chemlog',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     version='1.0.0',
     packages=['chemlog'],
     install_requires=[
@@ -17,6 +23,6 @@ setup(
     author='Simon Flügel',
     author_email='simon.fluegel@uos.de',
     description='Peptide classifier for ChEBI / PubChem',
-    license='MIT',
+    license='GNU General Public License v3.0',
 
 )
