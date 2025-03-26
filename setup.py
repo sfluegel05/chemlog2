05da5ee75df1d609edb5e5163a95459d6bc7d0bc
sfluegel <simon.fluegel@uni-osnabrueck.de>
2025-03-26 18:32:12 +0100

    bump version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name='chemlog',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version='1.0.0',
+    version='1.0.2',
     packages=['chemlog'],
     install_requires=[
         'fastobo',
