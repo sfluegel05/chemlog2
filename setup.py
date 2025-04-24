3fb7a3595be83c73b4f349e88d3e879021f731f7
Simon Flügel <43573433+sfluegel05@users.noreply.github.com>
2025-04-24 15:07:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name='chemlog',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version='1.0.3',
+    version='1.0.4',
     packages=find_packages(),
     install_requires=[
         'fastobo',
