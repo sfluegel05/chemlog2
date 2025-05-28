d056a846f7b280a326bf9611c765dfd796710bac
sfluegel <simon.fluegel@uni-osnabrueck.de>
2025-05-28 10:50:56 +0200

    fix tmp files for multiprocessing

--- chemlog/qbf_classification/qbf_solver.py
+++ chemlog/qbf_classification/qbf_solver.py
@@ -2,30 +2,32 @@ import logging
 import subprocess
 import time
 from typing import List
+import os
 
 DEPQBF_PATH = "./depqbf" # solver #"/../../Downloads/depqbf-version-6.03/depqbf-version-6.03/depqbf"
 BLOQQER_PATH = "./bloqqer" # preprocessor for qbf
 
 def qbf_solver(input: List[str]):
-    with open("tmp.qdimacs", "w") as f:
+    pid = os.getpid()
+    with open(os.path.join("tmp", f"{pid}.qdimacs"), "w") as f:
         f.write("\n".join(input))
     bloqqer_start = time.perf_counter()
     logging.debug(f"Preprocessing with Bloqqer")
     res_bloqqer = subprocess.run(
-        [BLOQQER_PATH, "tmp.qdimacs"],
+        [BLOQQER_PATH, os.path.join("tmp", f"{pid}.qdimacs")],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         universal_newlines=True,
     )
     logging.debug(f"Bloqqer finished in {time.perf_counter() - bloqqer_start:.2f} seconds")
 
-    with open("tmp_bloqqed.qdimacs", "w") as f:
+    with open(os.path.join("tmp", f"{pid}_bloqqed.qdimacs"), "w") as f:
         f.write(res_bloqqer.stdout)
 
     logging.debug(f"Running DepQBF on preprocessed file")
     depqbf_start = time.perf_counter()
     res = subprocess.run(
-        [DEPQBF_PATH, "tmp_bloqqed.qdimacs"],
+        [DEPQBF_PATH, os.path.join("tmp", f"{pid}_bloqqed.qdimacs")],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         universal_newlines=True,
