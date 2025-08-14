0b57945ff4eb35831d4906a55c08e4012938ba17
sfluegel <simon.fluegel@uni-osnabrueck.de>
2025-08-14 15:02:52 +0200

    add algorithmic classifier for peptide size that uses translation from msol

--- chemlog/fol_to_py/folToPyTranslator.py
+++ chemlog/fol_to_py/folToPyTranslator.py
@@ -2,23 +2,28 @@ import inspect
 
 from gavel.dialects.base.compiler import Compiler
 from gavel.logic import logic as fol
+from chemlog.msol.msol import Var2
 from rdkit import Chem
 from chemlog.msol.peptide_size import MSOLDefinition
 
+SO_KEY = "so_elements"
 PREDICATE_NAME_RDKIT_MAPPING = {
     "has_bond_to": lambda a, b: f"mol.GetBondBetweenAtoms({a}.GetIdx(), {b}.GetIdx()) is not None",
-    "bSINGLE": lambda a, b: f"mol.GetBondBetweenAtoms({a}.GetIdx(), {b}.GetIdx()).GetBondType() == Chem.BondType.SINGLE",
-    "bDOUBLE": lambda a, b: f"mol.GetBondBetweenAtoms({a}.GetIdx(), {b}.GetIdx()).GetBondType() == Chem.BondType.DOUBLE",
-    "bTRIPLE": lambda a, b: f"mol.GetBondBetweenAtoms({a}.GetIdx(), {b}.GetIdx()).GetBondType() == Chem.BondType.TRIPLE",
-    "bAROMATIC": lambda a, b: f"mol.GetBondBetweenAtoms({a}.GetIdx(), {b}.GetIdx()).GetBondType() == Chem.BondType.AROMATIC"
+    "bsingle": lambda a, b: f"(mol.GetBondBetweenAtoms({a}.GetIdx(), {b}.GetIdx()) is not None and mol.GetBondBetweenAtoms({a}.GetIdx(), {b}.GetIdx()).GetBondType() == Chem.BondType.SINGLE)",
+    "bdouble": lambda a, b: f"(mol.GetBondBetweenAtoms({a}.GetIdx(), {b}.GetIdx()) is not None and mol.GetBondBetweenAtoms({a}.GetIdx(), {b}.GetIdx()).GetBondType() == Chem.BondType.DOUBLE)",
+    "btriple": lambda a, b: f"(mol.GetBondBetweenAtoms({a}.GetIdx(), {b}.GetIdx()) is not None and mol.GetBondBetweenAtoms({a}.GetIdx(), {b}.GetIdx()).GetBondType() == Chem.BondType.TRIPLE)",
+    "baromatic": lambda a, b: f"(mol.GetBondBetweenAtoms({a}.GetIdx(), {b}.GetIdx()) is not None and mol.GetBondBetweenAtoms({a}.GetIdx(), {b}.GetIdx()).GetBondType() == Chem.BondType.AROMATIC)",
+    "is_in": lambda a, b: f"{a}.GetIdx() in {b}",
+    "buildingblock": lambda a: f"{a} in {SO_KEY}",
+    "chargen": lambda a: f"{a}.GetFormalCharge() < 0",
 }
 for i in range(1, 119):
     element = Chem.GetPeriodicTable().GetElementSymbol(i)
-    PREDICATE_NAME_RDKIT_MAPPING[element] = lambda a, element=element: f"{a}.GetSymbol() == '{element}'"
+    PREDICATE_NAME_RDKIT_MAPPING[element.lower()] = lambda a, element=element: f"{a}.GetSymbol() == '{element}'"
 for i in range(5):
-    PREDICATE_NAME_RDKIT_MAPPING[f"Has{i}Hs"] = lambda a, i=i: f"{a}.GetTotalNumHs() == {i}"
+    PREDICATE_NAME_RDKIT_MAPPING[f"has{i}hs"] = lambda a, i=i: f"{a}.GetTotalNumHs() == {i}"
 for charge in range(-3, 4):
-    PREDICATE_NAME_RDKIT_MAPPING[f"Charge{charge}"] = lambda a, charge=charge: f"{a}.GetFormalCharge() == {charge}"
+    PREDICATE_NAME_RDKIT_MAPPING[f"charge{charge}"] = lambda a, charge=charge: f"{a}.GetFormalCharge() == {charge}"
 
 class PythonCompiler(Compiler):
     """
@@ -56,7 +61,7 @@ class PythonCompiler(Compiler):
 
     def visit_quantified_formula(self, formula: fol.QuantifiedFormula):
         # for loop
-        return f"{self.visit(formula.quantifier)}({self.visit(formula.formula)} " + "".join(f"for {self.visit(v)} in mol.GetAtoms()" for v in formula.variables) + ")"
+        return f"{self.visit(formula.quantifier)}({self.visit(formula.formula)} " + " ".join(f"for {self.visit(v)} in {SO_KEY}" if isinstance(v, Var2) else f"for {self.visit(v)} in mol.GetAtoms()" for v in formula.variables) + ")"
 
     def visit_binary_formula(self, formula: fol.BinaryFormula):
         if formula.operator == fol.BinaryConnective.IMPLICATION:
@@ -65,13 +70,21 @@ class PythonCompiler(Compiler):
             return self.visit((~formula.left & ~formula.right) | (formula.left & formula.right))
         return f"({self.visit(formula.left)} {self.visit(formula.operator)} {self.visit(formula.right)})"
 
+    def visit_nary_formula(self, formula: fol.NaryFormula):
+        if len(list(formula.formulae)) == 0:
+            return True
+        if len(list(formula.formulae)) == 1:
+            return self.visit(list(formula.formulae)[0])
+        return f"({(' ' + self.visit(formula.operator) + ' ').join(self.visit(f) for f in formula.formulae)})"
+
     def visit_predicate_expression(self, expression: fol.PredicateExpression):
-        if expression.predicate in PREDICATE_NAME_RDKIT_MAPPING:
-            return PREDICATE_NAME_RDKIT_MAPPING[expression.predicate](*map(self.visit, expression.arguments))
+        # matching to mapping is not case-sensitive
+        if expression.predicate.lower() in PREDICATE_NAME_RDKIT_MAPPING:
+            return PREDICATE_NAME_RDKIT_MAPPING[expression.predicate.lower()](*map(self.visit, expression.arguments))
         else:
             # assume that the predicate is a python function defined (or translated) elsewhere
             # always pass the mol as first argument
-            return f"{expression.predicate}(mol, {', '.join(self.visit(arg) for arg in expression.arguments)})"
+            return f"{expression.predicate}(mol, {SO_KEY}, {', '.join(self.visit(arg) for arg in expression.arguments)})"
 
     def visit_variable(self, variable: fol.Variable):
         return variable.symbol
@@ -81,7 +94,8 @@ class PythonCompiler(Compiler):
 
     def visit_msol_definition(self, definition: MSOLDefinition):
         """
-        Visit a MSOL definition and return the Python code for it.
+        Visit a MSOL definition and return the Python code for it. (Note that the compiler can't actually handle
+        MSOL formulas, this is only the definition structure)
         """
         name = definition.name()
         # get signature of definition
